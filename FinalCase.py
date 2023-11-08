d98f6606be77fea89bee215e5f0a8f39e29a0fb5
Wolf-H-1997 <146092152+Wolf-H-1997@users.noreply.github.com>
2023-11-09 00:18:21 +0100

    Add files via upload

--- FinalCase.py
+++ FinalCase.py
@@ -16,6 +16,7 @@ from streamlit_folium import folium_static
 import plotly.express as px
 import streamlit as st
 import plotly.graph_objects as go
+import sklearn
 from plotly.subplots import make_subplots
 from sklearn.metrics import r2_score
 
